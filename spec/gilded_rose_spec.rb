ddaf958d15e538d1b7d9338fe46ba0ddb3065ddb
Peter Stevens <Peterstevens510@gmail.com>
2020-04-24 21:35:05 +0100

    Finished writing all tests for original file, just got to write tests for new method

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -2,69 +2,97 @@ require 'gilded_rose'
 require 'items'
 
 describe GildedRose do
-  # Item.new("food name", "sell_in value", "quality")
+  
   describe "#update_quality" do
     
-    it "does not change the name" do
-      items = [Item.new("Peter's Orange Juice", 10, 20)]
-      GildedRose.new(items).update_quality()
-      expect(items[0].name).to eq "Peter's Orange Juice"
+    context "For a normal item that isn't special." do
+      it "does not change the name" do
+        items = [Item.new("Peter's Orange Juice", 10, 20)]
+        GildedRose.new(items).update_quality()
+        expect(items[0].name).to eq "Peter's Orange Juice"
+      end
     end
+    
+    it "changes the Sell-in value by 1" do 
+      items = [Item.new("Peter's Orange Juice", 10, 20)] 
+      expect { GildedRose.new(items).update_quality() }.to change { items[0].sell_in }.by(-1)
+    end
+    
+    it "changes the quality by 1" do 
+      items = [Item.new("Peter's Orange Juice", 10, 20)] 
+      expect { GildedRose.new(items).update_quality() }.to change { items[0].quality }.by(-1)
+    end
+    
+    it "checks that if the sell by date has passed, quality degrades twice as fast" do
+      items = [Item.new("Peter's Orange Juice", 0, 20)] 
+      GildedRose.new(items).update_quality()
+      expect(items[0].quality).to eq(18)
+    end
+    
+    it "Shows that the quality of an item can never be negative" do 
+      items = [Item.new("Peter's Orange Juice", 10, 1)] 
+      5.times do 
+        GildedRose.new(items).update_quality()
+      end
+      expect(items[0].sell_in).to eq(5)
+      expect(items[0].quality).to be >= 0
+    end
+    
+    context "For the item 'Aged Brie'." do
+      it "Shows that 'Aged Brie' increases in Quality by 1 the older it gets" do
+        items = [Item.new("Aged Brie", 10, 10)] 
+        GildedRose.new(items).update_quality()
+        expect(items[0].sell_in).to eq(9)
+        expect(items[0].quality).to eq(11)
+      end 
+    end
+    
+    it "Shows that the quality of an item is never more than 50" do 
+      items = [Item.new("Aged Brie", 30, 30)]
+      21.times do
+      GildedRose.new(items).update_quality()
+    end
+    expect(items[0].sell_in).to eq(9)
+    expect(items[0].quality).to be <= 50
   end
-
-  it "changes the Sell-in value by 1" do 
-    items = [Item.new("Peter's Orange Juice", 10, 20)] 
-    expect { GildedRose.new(items).update_quality() }.to change { items[0].sell_in }.by(-1)
-  end
-
-  it "changes the quality by 1" do 
-    items = [Item.new("Peter's Orange Juice", 10, 20)] 
-    expect { GildedRose.new(items).update_quality() }.to change { items[0].quality }.by(-1)
-  end
-
-  it "checks that if the sell by date has passed, quality degrades twice as fast" do
-    items = [Item.new("Peter's Orange Juice", 0, 20)] 
-    GildedRose.new(items).update_quality()
-    expect(items[0].quality).to eq(18)
-  end
-
-  it "Proves 'Sulfuras', never have to be sold or decrease in Quality" do 
-    items = [Item.new("Sulfuras, Hand of Ragnaros", 10, 10)] 
-    GildedRose.new(items).update_quality()
-    expect(items[0].sell_in).to eq(10)
-    expect(items[0].quality).to eq(10)
-  end
-
-  it "Proves that 'Aged Brie' increases in Quality the older it gets" do
-    items = [Item.new("Aged Brie", 10, 10)] 
-    expect { GildedRose.new(items).update_quality() }.to change { items[0].quality }.by(1)
-  end 
   
-  it "Shows that the quality of an item can never be negative" do 
-    items = [Item.new("Peter's Orange Juice", 10, 1)] 
-    5.times do 
+  context "For the item 'Sulfuras, Hand of Ragnaros'." do  
+    it "Proves 'Sulfuras', never have to be sold or decrease in Quality" do 
+      items = [Item.new("Sulfuras, Hand of Ragnaros", 10, 10)] 
       GildedRose.new(items).update_quality()
+      expect(items[0].sell_in).to eq(10)
+      expect(items[0].quality).to eq(10)
     end
-    expect(items[0].sell_in).to eq(5)
-    expect(items[0].quality).to be >= 0
-    p items[0].sell_in
-    p items[0].quality
+    
+    context "For 'Backstage passes to a TAFKAL80ETC concert'." do  
+      it "Checks Backstage item increases in quality as it approaches sell-in date" do 
+        items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 15, 10)] 
+        GildedRose.new(items).update_quality()
+        expect(items[0].sell_in).to eq(14)
+        expect(items[0].quality).to eq(11)
+      end
+      
+      it "Checks Backstage item increases in quality by 2 when there are 10 days or less" do 
+        items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 10, 10)]
+        GildedRose.new(items).update_quality()
+        expect(items[0].sell_in).to eq(9)
+        expect(items[0].quality).to eq(12)
+      end 
+
+      it "Checks Backstage item increases in quality by 3 when there are 5 days or less" do 
+        items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 5, 10)]
+        GildedRose.new(items).update_quality()
+        expect(items[0].sell_in).to eq(4)
+        expect(items[0].quality).to eq(13)
+       end 
+
+       it "Checks Backstage item quality is 0 after the concert date" do 
+        items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 0, 10)]
+        GildedRose.new(items).update_quality()
+        expect(items[0].quality).to eq(0)
+      end 
+    end
   end
 
-  it "Shows that the quality of an item is never more than 50" do 
-    items = [Item.new("Peter's Orange Juice", 10, 1)] 
-
-
+  end
 end
-
-
-# All items have a SellIn value which denotes the number of days we have to sell the item. 
-# All items have a Quality value which denotes how valuable the item is. 
-
-# The Quality of an item is never more than 50
-
-# “Backstage passes”, like aged brie, increases in Quality as it’s 
-# SellIn value approaches; Quality increases by 2 when there are 10 days
-#  or less 
-# And by 3 when there are 5 days or less 
-# But Quality drops to 0 after the concert
