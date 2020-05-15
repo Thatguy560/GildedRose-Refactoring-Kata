90009f98b56d42bef7e4e94e46bff8d82905fc8c
Peter Stevens <Peterstevens510@gmail.com>
2020-05-15 15:54:46 +0100

    written tests for item class, now focusing on guilded rose

---
+++ spec/items_spec.rb
@@ -0,0 +1,12 @@
+require 'items'
+require 'gilded_rose'
+
+describe Item do  
+  
+  it "every item has a unique name, sell_in and quality." do
+    item = Item.new("Peter's Orange Juice", 20, 20) 
+    expect(item.name).to eq("Peter's Orange Juice")
+    expect(item.sell_in).to eq(20)
+    expect(item.quality).to eq(20)
+  end
+end
